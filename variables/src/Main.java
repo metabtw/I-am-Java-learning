b62af248426178599c233bdee47c1b4c0cf66754
btwmeta <137120250+metabtw@users.noreply.github.com>
2025-04-29 00:48:11 +0300

    javaLearnVariables

--- variables/src/Main.java
+++ variables/src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String name = "muhammed";
+        System.out.println(name);
 
+        int myAge = 19;
+        System.out.println(myAge);
+
+        float myFloatLength = 1.85f;
+        System.out.println(myFloatLength);
+
+        char myLetter = 'M';
+        System.out.println(myLetter);
     }
 }
