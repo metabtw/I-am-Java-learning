0cc63d3f40e7ccd53ba859d739e21bb66eb69b25
btwmeta <137120250+metabtw@users.noreply.github.com>
2025-04-30 22:47:57 +0300

    loops

---
+++ loops/src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //for
+
+        for(int i=1;i<10;i++){
+            System.out.println(i);
+        }
+        System.out.println("for döngüsü bitti");
+
+
+        // while
+        int i=1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("while döngüsü bitti");
+
+
+        //do-while
+
+
+        int j=1;
+        do {
+            System.out.println(j);
+            j++;
+        }while (j<10);
+        System.out.println("do-while döngüsü bitti");
+
+    }
+}
