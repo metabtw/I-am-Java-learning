cb4f8bcffe804d0566c9ba481866efac9dd6e0f4
btwmeta <137120250+metabtw@users.noreply.github.com>
2025-05-06 15:40:34 +0300

    kalin-ince-sesli-harf/miniProject2

---
+++ kalinSesliHarf/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        char harf = 'E';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            default:
+                System.out.println("İnce sesli harf");
+        }
+    }
+}
